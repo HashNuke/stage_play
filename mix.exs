d7b430c28cc84012085b051cfb499bebb0f99e0d
Akash Manohar J <akash@akash.im>
2017-04-26 08:12:38 +0530

    Add httpoison to dependencies

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule StagePlay.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:gen_stage, "~> 0.11"}]
+     {:gen_stage, "~> 0.11"},
+     {:httpoison, "~> 0.11.2"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
